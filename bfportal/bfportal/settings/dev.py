829e94127a1e38b030defe7794e8941f35f2c710
p0lygun <solankivibhakar82@gmail.com>
2022-01-11 14:59:36 +0530

    init wagtail commit

---
+++ bfportal/bfportal/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-z48ds*00ni*w)nn#3v5ehswftnsoh4sn5w)y2m7ux*4d!3k00t'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
