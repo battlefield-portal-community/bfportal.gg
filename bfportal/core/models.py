bdefb43896be7f5b304991f6c5a74e0b3ce1f00d
p0lygun <solankivibhakar82@gmail.com>
2022-01-16 14:34:37 +0530

    added handling for after submission and login/logout

--- bfportal/core/models.py
+++ bfportal/core/models.py
@@ -1,13 +1,37 @@
-from pyexpat import model
 from django.db import models
+from django.template.response import TemplateResponse
 from django.shortcuts import render
 
 from wagtail.core.models import Page
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.contrib.routable_page.models import RoutablePageMixin, route
 
-class IndexPage(Page):
+from allauth.socialaccount.models import SocialAccount
+
+class IndexPage(RoutablePageMixin, Page):
     max_count = 1
     parent_page_types = ['wagtailcore.Page']
+    subpage_types = ['core.ExperiencePage']
+
+    @route(r'^$')
+    def base(self, request):
+        return TemplateResponse(
+            request,
+            self.get_template(request),
+            self.get_context(request)
+        )
+    
+    @route(r'^submit/$')
+    def submit_exp(self, request):
+        from core.views import sumbit_experience
+        return sumbit_experience(request, self)
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+        if request.user.is_authenticated:
+            context['social_user'] = SocialAccount.objects.get(user=request.user)
+        return context
+
 
 class ExperiencePage(Page):
     name = models.CharField(max_length=255, default='')
--- bfportal/core/views.py
+++ bfportal/core/views.py
@@ -1,28 +1,29 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.template.defaultfilters import slugify
+from django.views.generic import TemplateView
 from wagtail.core.models import Page
 
+from loguru import logger
+
 from core.forms import ExperiencePageForm
-from core.models import ExperiencePage, IndexPage
+from core.models import ExperiencePage
 
 
-
-def sumbit_experience(request):
+@login_required
+def sumbit_experience(request, index_page: Page):
     if request.method == 'POST':
         form = ExperiencePageForm(request.POST)
         if form.is_valid():
-            new_exp = ExperiencePage(
-                title = form.cleaned_data['name'],
-                name = form.cleaned_data['name'],
-                code = form.cleaned_data['code'],
-                exp_url = form.cleaned_data['exp_url'],
-                vid_url = form.cleaned_data['vid_url'],
-                cover_img_url = form.cleaned_data['cover_img_url'],
-                description = form.cleaned_data['description'],
-                no_players = form.cleaned_data['no_players'],
-                no_bots = form.cleaned_data['no_bots'],
-            )
-            IndexPage.objects.first().add_child(instance=new_exp)
-            
+            logger.debug(f"Saving new exp {form.cleaned_data}")
+            new_exp_page : ExperiencePage = form.save(commit=False)
+            new_exp_page.slug = slugify(new_exp_page.name)
+            new_exp_page.title = new_exp_page.name
+            new_exp: ExperiencePage = index_page.add_child(instance=new_exp_page)
+            if new_exp:
+                new_exp.unpublish()
+                new_exp.save_revision(submitted_for_moderation=True, user=request.user)
+            return render(request, 'core/after_submit.html', {'exp_name': new_exp.name})
 
     else:
         form = ExperiencePageForm()
@@ -30,3 +31,4 @@ def sumbit_experience(request):
     return render(request, 'core/submit_experience_page.html', {
         'form': form,
     })
+
