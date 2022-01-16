5428ba0bb61588d05d2ffede3447d502f0721480
p0lygun <solankivibhakar82@gmail.com>
2022-01-16 14:35:05 +0530

    addded logging config

--- bfportal/manage.py
+++ bfportal/manage.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 import os
 import sys
+from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
+from loguru import logger
 
 if __name__ == "__main__":
+    logger.remove()
+    logger.add(
+        sys.stdout,
+        colorize=True,
+        format="[ <lr>bfportal</> ]"
+        "[<b><fg #3b3b3b>{level: ^8}</></>]"
+        "[{name}.{function}:{line}]"
+        "[ {message} ]",
+        level="DEBUG",
+    )
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bfportal.settings.dev")
 
     from django.core.management import execute_from_command_line
