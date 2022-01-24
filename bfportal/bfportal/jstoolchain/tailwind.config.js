819bfad6cd650a0e91b524d6a75003c14d00cfa8
p0lygun <solankivibhakar82@gmail.com>
2022-01-24 22:46:31 +0530

    added tailwind for css framework

---
+++ bfportal/bfportal/jstoolchain/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["../templates/**/*.{html,js}", "../../core/templates/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'discord-dark' : "#36393f",
+      }
+    },
+  },
+  plugins: [],
+}
