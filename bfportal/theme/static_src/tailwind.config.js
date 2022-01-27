d69fd645325fc758a41bd8b1dcd36373bd26dda4
p0lygun <solankivibhakar82@gmail.com>
2022-01-27 18:15:57 +0530

    added bfportal.gg old color scheme

--- bfportal/theme/static_src/tailwind.config.js
+++ bfportal/theme/static_src/tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
          */
         '../../templates/**/*.html',
         
-        /* 
+        /*
          * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
          * Adjust the following line to match your project structure.
          */
@@ -43,17 +43,29 @@ module.exports = {
     ],
     theme: {
         extend: {
-      colors: {
-          'discord-dark': "#303434",
-          'discord-light-dark' : "#36393f",
-          'bf2042-1' : "#011C26",
-          'bf2042-2' : "#025159",
-          'bf2042-3' : "#08A696",
-          'bf2042-4' : "#26FFDF",
-          'bf2042-5' : "#F26A1B",
-          'bf2042-6' : "#FF2C10",
-      },
-      }
+            colors: {
+                'bg-default' : '#121212',
+                'card-bg' : '#262626',
+                'hover-bg-light': '#474c50',
+                'accent-bg' : '#303030',
+                'discord-dark': "#303434",
+                'discord-light-dark': "#36393f",
+                'bf2042-1': "#011C26",
+                'bf2042-2': "#025159",
+                'bf2042-3': "#08A696",
+                'bf2042-4': "#26FFDF",
+                'bf2042-5': "#F26A1B",
+                'bf2042-6': "#FF2C10",
+            },
+            dropShadow: {
+                'bf2042-1' : '0 0 0.35rem #011C26',
+                'bf2042-2' : '0 0 0.35rem #025159',
+                'bf2042-3' : '0 0 0.35rem #08A696',
+                'bf2042-4' : '0 0 0.35rem #26FFDF',
+                'bf2042-5' : '0 0 0.35rem #F26A1B',
+                'bf2042-6' : '0 0 0.35rem #FF2C10',
+            },
+        }
     },
     plugins: [
         /**
