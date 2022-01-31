1f5d6034f300edf812f18052efaa3e45f21ef327
p0lygun <solankivibhakar82@gmail.com>
2022-01-31 15:11:12 +0530

    add pagination_suffix tag; subtract filter;

--- bfportal/core/templatetags/template_filters.py
+++ bfportal/core/templatetags/template_filters.py
@@ -1,5 +1,6 @@
 from django.template.defaulttags import register
 from loguru import logger
+import re
 
 
 @register.filter
@@ -16,3 +17,30 @@ def get_social_account(user):
             return all_acc[0]
     else:
         return None
+
+
+@register.filter(name='sub')
+def subtract(value, arg):
+    return value - arg
+
+
+@register.simple_tag
+def pagination_suffix(value):
+    """
+    returns url for paginator suffix
+    """
+    """
+        sample 
+            [Input] : tag=management&tag=since&page=3
+            [Output] : &tag=management&tag=since
+    """
+    suffix_params = re.sub(r"[&?]?page=\d+", '', value)
+    if len(suffix_params):
+        return ('&'+suffix_params).replace("&&", '&')
+    else:
+        return ''
+
+
+@register.filter(name='abs')
+def abs_filter(value):
+    return abs(value)
