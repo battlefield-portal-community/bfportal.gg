bf48629087ea6e457063e5d9a469baa753f2f121
p0lygun <solankivibhakar82@gmail.com>
2022-02-18 21:35:48 +0530

    add webhook binding to send embeds to discord on experience submit and edit

--- bfportal/core/views.py
+++ bfportal/core/views.py
@@ -1,9 +1,16 @@
+import datetime
+import os
+
+from allauth.socialaccount.models import SocialAccount
 from dal import autocomplete
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.conf import settings
+from django.utils import timezone
 from loguru import logger
 from taggit.models import Tag
+import requests
 
 from core.forms import ExperiencePageForm
 from core.models import (
@@ -13,6 +20,105 @@ from core.models import (
     ExperiencesCategory,
 )
 from core.utils.helper import unique_slug_generator
+
+
+def send_approve_request(page: ExperiencePage, type: str = "new"):
+    logger.debug("send_approve_request called")
+    if (token := os.getenv('APPROVAL_CHANNEL_WEBHOOK_TOKEN', None)) is not None:
+        webhook_id = os.getenv("APPROVAL_CHANNEL_WEBHOOK_ID") if not settings.DEBUG else "944252843009917039"
+        webhook_url = f"https://discord.com/api/webhooks/{webhook_id}/{token}"
+        uid = SocialAccount.objects.get(user_id=page.owner.id).uid
+        data = dict()
+        if type == "new":
+            data = {
+                "content": "New Experience approve request",
+                "embeds": [
+                    {
+                        "url": page.full_url,
+                        "title": page.title,
+                        "description": page.description[0:200] + ".....",
+                        "image": {
+                            "url": "https://super-static-assets.s3.amazonaws.com/19d9fbc6-6292-4be8-ac70-5a186b556054%2Fimages%2Fb6495922-b4c7-4002-9c3d-56bfaa5b98b5.jpg"
+                        },
+                        "fields": [
+                            {
+                                "name": "Author",
+                                "value": f"<@{uid}>",
+                                "inline": True
+                            },
+                            {
+                                "name": "Submitted on",
+                                "value": f"<t:{int(page.first_published_at.timestamp())}>",
+                                "inline": True
+                            },
+                            {
+                                "name": "Featured",
+                                "value": ":white_check_mark:" if page.featured else ":x:",
+                                "inline": True
+                            },
+                            {
+                                "name": "Category",
+                                "value": ":white_small_square: " + "\u200B".join(
+                                    [str(i) for i in page.categories.all()]),
+                            },
+                            {
+                                "name": "Tags",
+                                "value": ":white_small_square: " + "".join([f"`{i}` " for i in page.tags.all()]),
+                            },
+                        ]
+                    }
+                ]
+            }
+        elif type == "edit":
+            data = {
+                "content": "Request to make Changes to a Experience",
+                "embeds": [
+                    {
+                        "url": page.full_url,
+                        "title": page.title,
+                        "fields": [
+                            {
+                                "name": "By",
+                                "value": f"<@{uid}>"
+                            }
+                        ]
+
+                    }
+                ]
+            }
+        else:
+            data = {
+                "content": "There is Something Wrong with the server pls check 🤐"
+            }
+
+        data["token"] = token
+        data["components"] = [
+            {
+                "type": 1,
+                "components": [
+                    {
+                        "type": 2,
+                        "style": 5,
+                        "url": page.full_url,
+                        "label": "Go to Dashboard",
+
+                    }
+                ]
+            }
+        ]
+
+        print(data)
+        headers = {
+            "Content-Type": "application/json"
+        }
+        result = requests.post(webhook_url, json=data, headers=headers)
+        try:
+            result.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            logger.debug(f"Error {err} while sending approve request for {page.title}")
+            print(result.content)
+        else:
+            logger.debug(f"{type.capitalize()} Experience request for {page.title} sent successfully ")
 
 
 @login_required
@@ -36,6 +142,9 @@ def submit_experience(request: HttpRequest, home_page: HomePage):
             if new_exp:
                 new_exp.unpublish()
                 new_exp.save_revision(submitted_for_moderation=True, user=request.user)
+                new_exp.first_published_at = timezone.now()
+                new_exp.save()
+                send_approve_request(new_exp, "new")
             return render(
                 request, "core/after_submit.html", {"exp_name": new_exp.title}
             )
@@ -65,6 +174,7 @@ def edit_experience(request: HttpRequest, experience_page: ExperiencePage):
             experience_page.save_revision(
                 submitted_for_moderation=True, user=request.user, changed=True
             )
+            send_approve_request(experience_page, "edit")
             return render(
                 request,
                 "core/after_submit.html",
@@ -78,6 +188,8 @@ def edit_experience(request: HttpRequest, experience_page: ExperiencePage):
                 "form": ExperiencePageForm(instance=experience_page),
             },
         )
+
+
 # to sort tags based on how many time its used
 # >>> a = ExperiencePageTag.objects.values_list('tag_id').annotate(tag_count=Count('tag_id')).order_by('-tag_count')
 # >>> [Tag.objects.get(id=i[0]) for i in a]
