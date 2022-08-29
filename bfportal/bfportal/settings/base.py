9b93e5192403df6da94b28aa533c409aed5746fe
p0lygun <solankivibhakar82@gmail.com>
2022-08-29 18:54:31 +0530

    feat: remove whitenoise; nginx will server static

--- bfportal/bfportal/settings/base.py
+++ bfportal/bfportal/settings/base.py
@@ -89,7 +89,6 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
     "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
