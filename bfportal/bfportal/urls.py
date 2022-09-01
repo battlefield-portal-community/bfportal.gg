0fd16a23c0692c4f2f26952603be8db0a93c0aa9
Vibhakar Solanki <solankivibhakar82@gmail.com>
2022-09-01 17:08:40 +0530

    feat: add ability to transfer ownership of a page (#60)

--- bfportal/bfportal/urls.py
+++ bfportal/bfportal/urls.py
@@ -6,9 +6,11 @@ from django.views.generic import RedirectView, TemplateView
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.core import urls as wagtail_urls
 from wagtail.documents import urls as wagtaildocs_urls
+from wagtailautocomplete.urls.admin import urlpatterns as autocomplete_admin_urls
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
+    path("admin/autocomplete/", include(autocomplete_admin_urls)),
     path("admin/", include(wagtailadmin_urls)),
     path("documents/", include(wagtaildocs_urls)),
     # allow only social login :)
