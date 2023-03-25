142beab16e9e947456184e5624ac47599115bd04
Vibhakar Solanki <solankivibhakar82@gmail.com>
2023-03-25 17:32:13 +0530

    fix: Add CSRF_TRUSTED_ORIGINS to settings.py (#165)
    * feat: add github sponsors info
    
    * fix: add CSRF_TRUSTED_ORIGINS to settings
    
    * feat: add WIP action to manage PRs

--- bfportal/bfportal/settings/production.py
+++ bfportal/bfportal/settings/production.py
@@ -8,6 +8,7 @@ DEBUG = True if os.environ.get("DEBUG", "False") == "True" else False
 SECRET_KEY = os.environ.get("PRODUCTION_KEY")
 ALLOWED_HOSTS = os.environ.get("PRODUCTION_HOST").split(",")
 ALLOWED_HOSTS.append("localhost")
+CSRF_TRUSTED_ORIGINS = ["https://*.bfportal.gg/"]
 logger.debug(f"Allowed Hosts {ALLOWED_HOSTS}")
 
 try:
