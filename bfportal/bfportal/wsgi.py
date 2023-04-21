3e243488d81364f185c69ca6a75367b2761b8291
p0lygun <solankivibhakar82@gmail.com>
2023-04-22 01:56:39 +0530

    feat: supress wagtail warning and load .env when starting with gunicorn

--- bfportal/bfportal/wsgi.py
+++ bfportal/bfportal/wsgi.py
@@ -8,9 +8,30 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import warnings
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bfportal.settings.production")
+load_dotenv()
 
-application = get_wsgi_application()
+
+def init_project():
+    """Initialize the project
+
+    Used to load .env file and set project specific settings
+    """
+    from loguru import logger
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bfportal.settings.production")
+    logger.info(".env loaded")
+
+
+with warnings.catch_warnings():
+    from wagtail.utils.deprecation import RemovedInWagtail50Warning
+
+    warnings.filterwarnings(
+        "ignore", category=RemovedInWagtail50Warning
+    )  # supress only wagtail warnings
+    init_project()
+    application = get_wsgi_application()
