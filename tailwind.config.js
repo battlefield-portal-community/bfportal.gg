e472f7e7d1d48b12299092f955dcf7e45254bc02
p0lygun <solankivibhakar82@gmail.com>
2023-07-11 02:29:55 +0530

    feat(tailwind): add scripts to make dev experience better

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,39 +10,8 @@ module.exports = {
         /**
          * HTML. Paths to Django template files that will contain Tailwind CSS classes.
          */
-
-        /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
-        '../templates/**/*.html',
-
-        /*
-         * Main templates directory of the project (BASE_DIR/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../templates/**/*.html',
-
-        /*
-         * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../**/templates/**/*.html',
-        './node_modules/tw-elements/dist/js/**/*.js',
-        '../../**/static/js/*.js',
-
-        /**
-         * JS: If you use Tailwind CSS in JavaScript, uncomment the following lines and make sure
-         * patterns match your project structure.
-         */
-        /* JS 1: Ignore any JavaScript in node_modules folder. */
-        // '!../../**/node_modules',
-        /* JS 2: Process all JavaScript files in the project. */
-        // '../../**/*.js',
-
-        /**
-         * Python: If you use Tailwind CSS classes in Python, uncomment the following line
-         * and make sure the pattern below matches your project structure.
-         */
-        // '../../**/*.py'
-        //#36393f
+        'bfportal/**/templates/**/*.html',
+        'bfportal/**/static/js/*.js',
     ],
     theme: {
         extend: {
