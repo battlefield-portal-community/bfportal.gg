b073ec889082db384629b3eb5960aaeaa77ca265
MagneticNeedle <solankivibhakar82@gmail.com>
2026-07-01 22:45:36 +0530

    feat: register scripts models and add migration

--- bfportal/bf6/models/__init__.py
+++ bfportal/bf6/models/__init__.py
@@ -1,2 +1,5 @@
 # flake8: noqa F401
+from .categories import ScriptsCategory, ScriptsSubCategory
 from .home_page import BF6HomePage
+from .scripts_page.listing_page import ScriptsListingPage
+from .scripts_page.snippets import SnippetPage
